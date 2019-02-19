2e0ccf5a2d0b6c43235841e68563f69a605d30fe
tuius <tuiusx@icloud.com>
2019-02-19 19:32:07 -0300

    vs1

---
+++ captura.py
@@ -0,0 +1,33 @@
+import cv2
+
+classificador = cv2.CascadeClassifier("haarcascade-frontalface-default.xml")
+camera = cv2.VideoCapture(0)
+amostra = 1
+numeroAmostra = 25
+id = input('Digite seu identificador: ')
+largura, altura  = 220, 220
+print("Capturando as faces....")
+
+
+while (True):
+    conectado, imagem = camera.read()
+    imagemCinza = cv2.cvtColor(imagem, cv2.COLOR_BGR2GRAY)
+    facesDetectadas = classificador.detectMultiScale(imagemCinza, scaleFactor=1.5, minSize=(150, 150))
+
+    for (x, y, l, a) in facesDetectadas:
+        cv2.rectangle(imagem, (x, y), (x+l, y+a), (0, 0, 255), 2)
+        if cv2.waitKey (l) & 0xff == ord('q'):
+            imagemFace = cv2.resize(imagemCinza[y:y + a, x:x +l],(largura, altura))
+            cv2.imwrite("fotos/pessoa." + str (id) + "." + str(amostra) + ".jpg", imagemFace)
+            print("[foto" + str (amostra) + " capturada com sucesso]")
+            amostra += 1
+
+
+    cv2.imshow("Face", imagem)
+    cv2.waitKey(1)
+    if (amostra >= numeroAmostra +1):
+        break
+
+camera.release()
+cv2.destroyAllWindows()
+
